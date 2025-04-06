2d8432f36c3250d750cf2eb23e36db9ab48f25a3
Advait Desai <advaitdesai0702@gmail.com>
2025-04-06 05:37:58 +0530

    added route for ngo registration

--- app/routes/register.py
+++ app/routes/register.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.models.db import users_collection, events_collection
+from app.models.db import users_collection, events_collection, ngos_collection
 from datetime import datetime
 from bson import ObjectId
 
@@ -55,4 +55,47 @@ def register_event():
 
 @register_bp.route('/ngo', methods=['POST'])
 def register_ngo():
-    return "Register NGO route initialized"
+    data = request.get_json()
+    if not data:
+        return "No data provided", 400
+    
+    ngo_id = data.get('ngo_id')
+    if not ngo_id:
+        return jsonify({"status": "error", "message": "No event_id provided"}), 400
+    
+    auth_header = request.headers.get('Authorization')
+    token = auth_header.split(' ')[1]
+    print(token)
+    if not token:
+        return jsonify({"error": "Missing session token"}), 401
+    user_data = users_collection.find_one({
+        'session_token': token,
+        'session_expires': {'$gt': datetime.utcnow()}
+    })
+    
+    user_id = user_data.get('_id') if user_data else None
+    print(user_id)
+    if not user_id:
+        return jsonify({"error": "Invalid session token"}), 401
+
+    if not user_id or not ObjectId.is_valid(user_id):
+        return jsonify({"error": "Invalid or missing user_id"}), 400
+    
+    ngo = ngos_collection.find_one({'_id': ObjectId(ngo_id)})
+    if not ngo:
+        return jsonify({"error": "NGO not found"}), 404
+
+    if user_id in ngo.get('members', []):
+        return jsonify({"message": "User already registered for the NGO"}), 400
+
+    ngos_collection.update_one(
+        {'_id': ObjectId(ngo_id)},
+        {'$push': {'members': user_id}}
+    )
+    
+    users_collection.update_one(
+        {'_id': ObjectId(user_id)},
+        {'$push': {"ngos_joined": ngo_id}}
+    )
+
+    return jsonify({"message": "User successfully registered for the NGO"}), 200
